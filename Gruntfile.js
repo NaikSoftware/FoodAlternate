b04c7f54ab970a7593e80ef01cadb3aa64c85d41
Naik <naiksav4enko@gmail.com>
2016-04-17 14:39:50 +0300

    Use grunt for build

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        babel: {
+            options: {
+                sourceMap: false,
+                presets: ['es2015', 'react']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'client/javascript/',
+                    src: ['**/*.jsx', '**/*.js'],
+                    dest: 'public/javascript/',
+                    ext: '.js'
+                }]
+            }
+        },
+        uglify: {
+            options: {
+                compress: true
+            },
+            compress: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/javascript/',
+                    src: ['**/*.js', '!**/*.min.js'],
+                    dest: 'public/javascript/',
+                    ext: '.min.js'
+                }]
+            }
+        },
+        cssmin: {
+            options: {
+                compress: true
+            },
+            compress: {
+                files: [{
+                    expand: true,
+                    cwd: 'client/stylesheets/',
+                    src: ['**/*.css', '!**/*.min.css'],
+                    dest: 'public/stylesheets/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        concat: {
+            options: {
+                separator: '\n'
+            },
+            concat_js: {
+                src: ['public/javascript/**/*.min.js'],
+                dest: 'public/javascript/bundle.min.js'
+            },
+            concat_css: {
+                src: ['public/stylesheets/**/*.min.css'],
+                dest: 'public/stylesheets/bundle.min.css'
+            }
+        },
+        processhtml: {
+            html_edit: {
+                files: {
+                    'templates/index.html': 'templates/index.html',
+                    'templates/header.html': 'templates/header.html'
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.registerTask('prod', ['babel', 'uglify', 'cssmin', 'concat']);
+};
---
+++ client/javascript/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by naik on 17.04.16.
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App.jsx'
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('mount-point')
+);
